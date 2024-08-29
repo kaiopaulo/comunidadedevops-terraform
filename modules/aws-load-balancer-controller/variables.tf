b8882b946c826cf9ac50a2543321360e47ee0676
kaiopaulo <kaio.paulo@hotmail.com>
2024-08-29 15:34:27 -0300

    feat: added controller module

--- modules/aws-load-balancer-controller/variables.tf
+++ modules/aws-load-balancer-controller/variables.tf
@@ -7,3 +7,11 @@ variable "tags" {
   type        = map(any)
   description = "Tags to be added to AWS resources"
 }
+variable "oidc" {
+  type        = string
+  description = "HTTPS URL from OIDC provider of the EKS Cluster"
+}
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+}
--- provider.tf
+++ provider.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.62.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
   }
 
   backend "s3" {
@@ -15,5 +23,27 @@ terraform {
 
 
 provider "aws" {
-  # Configuration options
+  region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
+  }
 }
